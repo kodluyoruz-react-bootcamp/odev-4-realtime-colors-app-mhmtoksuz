ffa254956c60ad57673b68e7c806a5edfcb91052
Mehmet Öksüz <mehmet.733m@gmail.com>
2021-01-25 23:32:23 +0300

    heroku yapıldı

--- src/services.js
+++ src/services.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 let socket;
 
 export const initSocket = () => {
-  socket = io("http://localhost:3000", {
+  socket = io("https://kodluyoruzodev.herokuapp.com", {
     transports: ["websocket"]
   });
   console.log("Connecting...");
